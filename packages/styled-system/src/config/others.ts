38785555d50810b61572e4f225e98dbc10908f80
Tim Kolberger <tim@kolberger.eu>
2021-01-08 00:29:59 +0100

    feat(theme-typings): simplify type concat

--- packages/styled-system/src/config/others.ts
+++ packages/styled-system/src/config/others.ts
@@ -1,8 +1,7 @@
 import * as CSS from "csstype"
 import { Config, createParser, PropConfig, system } from "../core"
 import { getIsRtl } from "../utils/directionality"
-import { Length, ResponsiveValue } from "../utils/types"
-import { ThemeTypings } from "../theming.types"
+import { Length, ResponsiveValue } from "../utils"
 
 const floatTransform: PropConfig["transform"] = (value, _, props = {}) => {
   const map = { left: "right", right: "left" }
@@ -58,7 +57,7 @@ const config: Config = {
   },
 }
 
-export interface OtherProps<Theme extends ThemeTypings = ThemeTypings> {
+export interface OtherProps {
   /**
    * The CSS `animation` property
    */
@@ -94,9 +93,7 @@ export interface OtherProps<Theme extends ThemeTypings = ThemeTypings> {
   /**
    * The CSS `object-position` property
    */
-  objectPosition?: ResponsiveValue<
-    Theme["sizes"] | CSS.Property.ObjectPosition<Length>
-  >
+  objectPosition?: ResponsiveValue<CSS.Property.ObjectPosition<Length>>
   /**
    * The CSS `float` property
    */
